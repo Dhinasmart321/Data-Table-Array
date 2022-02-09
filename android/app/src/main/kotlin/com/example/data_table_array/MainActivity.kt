399443b29a19a4895d42a6096f24a361eddec4d8
dhinasmart321 <elumalaijai2018@gmail.com>
2022-02-09 18:27:18 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/data_table_array/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.data_table_array
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> key1 = ['Profit', 'Loss', 'Tax', 'Income'];
+  List<String> Value1 = ['100', '300', '500', '200'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              elevation: 2,
+              child: Container(
+                alignment: Alignment.center,
+                child: DataTable(columns: [
+                  DataColumn(label: Text('Total Price')),
+                  DataColumn(label: Text('500')),
+                ], rows: [
+                  for (var i = 0; i < key1.length; i++)
+                    DataRow(cells: [
+                      DataCell(Text(key1[i])),
+                      DataCell(Text(Value1[i])),
+                    ])
+                ]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
